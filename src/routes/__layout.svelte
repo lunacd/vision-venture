7186ed16ed6f234855ca25fffbbce75b851ea01b
Haowen Liu <lhw@lunacd.com>
2022-03-18 15:25:47 -0700

    USC banner

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,32 @@
+<!--A __layout.svelte file defines a common layout for all routes.
+    The <slot/> tag defines where the route content should be placed.
+    Refer to https://svelte.dev/docs#template-syntax-slot
+-->
+
+<!-- This file defines the required USC banner that shows on the top of all pages -->
+
+<style lang="scss">
+  #usc-banner {
+    height: 60px;
+    background-color: $black;
+  }
+
+  #usc-banner a {
+    float: right;
+    height: 38px;
+    width: 176px;
+    padding: 11px;
+  }
+</style>
+
+<script>
+    import Logo from '../images/usc-shield-name-white.png?webp';
+</script>
+
+<div id="usc-banner">
+    <a href="https://www.usc.edu/" referrerpolicy="no-referrer">
+        <img src={Logo} alt="USC Logo"/>
+    </a>
+</div>
+
+<slot/>
---
+++ svelte.config.js
@@ -0,0 +1,31 @@
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import {imagetools} from 'vite-imagetools'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess({
+        defaults: {
+            style: 'scss',
+            script: 'typescript',
+            markup: 'html',
+        },
+        scss: {
+            prependData: `@import './src/styles/global.scss';`,
+        },
+    }),
+
+    kit: {
+        adapter: adapter(),
+    },
+
+    vite: {
+        plugins: [imagetools({
+            include: './images/**'
+        })],
+    },
+};
+
+export default config;
