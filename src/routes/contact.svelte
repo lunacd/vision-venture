e33c24fe368f5bd0371cee72fa495fb7573dcf72
Haowen Liu <lhw@lunacd.com>
2022-04-24 12:27:39 -0700

    Add contact us

---
+++ src/routes/contact.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import Container from '../components/container.svelte';
+	import TitleSection from '../components/title_section.svelte';
+</script>
+
+<svelte:head>
+	<title>Contact Us | Viterbi Vision Venture</title>
+</svelte:head>
+
+<TitleSection
+	name="contact"
+	title="Contact Us"
+	tagline="Get Involved."
+	description={[
+		'Interested in learning more about Vision Venture or participating in future seasons?  Email us!'
+	]}
+/>
+
+<Container >
+    <div class="email-container">
+        <a class="email" href="mailto:VisionVenture@usc.edu">
+            VisionVenture@usc.edu
+        </a>
+    </div>
+</Container>
+
+<style lang="scss">
+    .email-container {
+        display: flex;
+        justify-content: center;
+    }
+
+    .email {
+        text-align: center;
+        font-family: $sans;
+        font-size: 2rem;
+        text-decoration: none;
+        color: $cardinal;
+    }
+</style>
