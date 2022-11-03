22fd3f6c92b815f708b54fca8e2dc80f09e425f9
Haowen Liu <lhw@lunacd.com>
2022-11-03 12:40:28 -0700

    Add Viterbi logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,21 +5,33 @@
  -->
 <script lang="ts">
 	import '../app.css';
+	import Container from '../components/container.svelte';
 
 	import Logo from '../images/usc-shield-name-white.png?webp';
+	import Viterbi from '../images/viterbi.jpg?webp';
 
 	export const prerender = true;
 </script>
 
 <div class="h-[60px] bg-black">
-	<a
-		rel="external"
-		href="https://www.usc.edu/"
-		referrerpolicy="no-referrer"
-		class="float-right h-[60px] w-[198px] p-[11px]"
-	>
-		<img src={Logo} alt="USC Logo" />
-	</a>
+	<Container>
+		<a
+			rel="external"
+			href="https://viterbi.usc.edu/"
+			referrerpolicy="no-referrer"
+			class="float-left h-[60px] w-[171px]"
+		>
+			<img src={Viterbi} alt="USC Viterbi Logo" />
+		</a>
+		<a
+			rel="external"
+			href="https://www.usc.edu/"
+			referrerpolicy="no-referrer"
+			class="float-right h-[60px] w-[198px] p-[11px]"
+		>
+			<img src={Logo} alt="USC Logo" />
+		</a>
+	</Container>
 </div>
 
 <div class="relative w-screen overflow-x-hidden">
